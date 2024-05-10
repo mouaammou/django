4a27e381bea893b1dd606044c5dadc13ec1ccc50
mouaammou <mouadamassine@gmail.com>
2024-05-10 22:03:25 +0100

    I understand the static files perfectly

--- my_app_admin/my_app_admin/settings.py
+++ my_app_admin/my_app_admin/settings.py
@@ -117,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "productionfiles"
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+	BASE_DIR / 'global_static_files'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
